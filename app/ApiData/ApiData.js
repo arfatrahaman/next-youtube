bd053c62ddf6b84cf6b94cbb64b90a4a3f4151e5
Arfat Rahaman Hridoy <arfatrahamanh@gmail.com>
2023-06-20 20:08:52 +0600

    Key Change

--- app/ApiData/ApiData.js
+++ app/ApiData/ApiData.js
@@ -2,9 +2,7 @@ const apiUrl = "https://youtube-v31.p.rapidapi.com";
 const options = {
   method: "GET",
   headers: {
-    // "X-RapidAPI-Key": "827eb00ed9msh5a86f1c732828b8p17174djsn3d2fb52e996f",
-    // "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
-    "X-RapidAPI-Key": "430dc15a18mshc1e407ac4ce41b1p16b3efjsn156649de6e83",
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
